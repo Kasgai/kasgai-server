bdc7534472a7d3614c3f08a8b05dbc599ac766fb
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2018-11-03 00:14:57 +0900

    init kasgai

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kasgai-server
+
+## Usage
+
+```bash
+git clone https://github.com/Kasgai/kasgai-server
+cd kasgai-server
+git submodule update --init --recursive
+```
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+
+	"google.golang.org/appengine"
+)
+
+func main() {
+	Run()
+	appengine.Main()
+}
+
+func Run() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./kasgai/index.html")
+	})
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./kasgai/")))
+
+	http.Handle("/", handlers.CombinedLoggingHandler(os.Stderr, r))
+}
